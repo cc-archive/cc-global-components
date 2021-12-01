16ba47ca3cf5bf9aa0ef52994173892a1bf227fb
Muluh MG Godson <godsonmuluh@gmail.com>
2021-12-01 16:51:52 +0100

    render menu items via api

--- src/components/cc-global-header.vue
+++ src/components/cc-global-header.vue
@@ -6,22 +6,6 @@
             </div>
             <div class="navbar-menu is-active">
                 <div class="navbar-start">
-                    <!-- Locale options 
-                    <div class="locale">
-                        <div class="select">
-                            <select>
-                                <option disabled="">Select</option>
-                                <option selected="">English</option>
-                                <option>le Français</option>
-                                <option>日本語</option>
-                                <option>Русский</option>
-                                <option>Español</option>
-                            </select>
-                        </div>
-                        <span class="icon caret-down is-small is-right">
-                            <svg viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg"><path d="M7.45896 11.25H22.5351c1.043 0 1.5645 1.2598.8262 1.998l-7.5352 7.5411c-.457.457-1.2011.457-1.6582 0L6.63279 13.248c-.73828-.7382-.2168-1.998.82617-1.998z" fill="currentColor"></path></svg>
-                        </span>
-                    </div>-->
                     <!-- Donate button -->
                     <a class="button donate" href="https://creativecommons.org/donate?c_src=website&amp;c_src2=NavBar">
                         <i class="icon heart"></i>
@@ -38,58 +22,25 @@
             </div>
             <div :class="{ ['navbar-menu']: true, ['is-active']: isBurgerMenuActive }">
                 <div class="navbar-end">
-                    <!-- Use this Div for dropdown menu items -->
-                    <div class="navbar-item has-dropdown is-hoverable" ref="outerDiv" @mouseenter="openMenu" @mouseleave="closeMenu">
-                        <button class="navbar-link is-arrowless" aria-haspopup="menu" aria-expanded="false" ref="opener" @click="toggleMenu">
-                            Who we are
-                            <!--  <span class="icon"><CaretDown /></span>-->
-                            <i class="icon caret-down" />
-                        </button>
-                        <div class="navbar-dropdown">
-                            <a href="#" class="navbar-item navbar-link">Item 2-1</a>
-                            <a href="#" class="navbar-item navbar-link">Item 2-2</a>
-                            <a href="#" class="navbar-item navbar-link">Item 2-3</a>
-                            <a href="#" class="navbar-item navbar-link">Item 2-4</a>
+                     <div class="" v-for="menu in menus" :key="menu.ID">
+                        <a v-if="!menu.child_items" class="navbar_item navbar-link is-arrowless" tabindex="0" :href="menu.url">
+                            {{ menu.title }}
+                            <!--  <span class="icon"><ExternalLink /></span>-->
+                            <i class="icon external-link" v-if="isExternal" />
+                        </a>
+                        <div v-else class="navbar-item has-dropdown is-hoverable" ref="outerDiv" @mouseenter="openMenu" @mouseleave="closeMenu">
+                            <div>
+                                <button class="navbar-link is-arrowless" aria-haspopup="menu" aria-expanded="false" ref="opener" @click="toggleMenu">
+                                    {{ menu.title }}
+                                    <!--  <span class="icon"><CaretDown /></span>-->
+                                    <i class="icon caret-down" />
+                                </button>
+                                <div class="navbar-dropdown">
+                                    <a v-for="child_menu in menu.child_items" :key="child_menu.ID" :href="child_menu.url" class="navbar-item navbar-link">{{child_menu.title}}</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
-
-                    <div class="navbar-item has-dropdown is-hoverable" ref="outerDiv" @mouseenter="openMenu" @mouseleave="closeMenu">
-                        <button class="navbar-link is-arrowless" aria-haspopup="menu" aria-expanded="false" ref="opener" @click="toggleMenu">
-                            What we do
-                            <!--  <span class="icon"><CaretDown /></span>-->
-                            <i class="icon caret-down" />
-                        </button>
-                        <div class="navbar-dropdown">
-                            <a href="#" class="navbar-item navbar-link">Item 3-1</a>
-                            <a href="#" class="navbar-item navbar-link">Item 3-2</a>
-                        </div>
-                    </div>
-
-                    <div class="navbar-item has-dropdown is-hoverable" ref="outerDiv" @mouseenter="openMenu" @mouseleave="closeMenu">
-                        <button class="navbar-link is-arrowless" aria-haspopup="menu" aria-expanded="false" ref="opener" @click="toggleMenu">
-                            Licenses and tools
-                            <!--  <span class="icon"><CaretDown /></span>-->
-                            <i class="icon caret-down" />
-                        </button>
-                        <div class="navbar-dropdown">
-                            <a href="#" class="navbar-item navbar-link">Item 3-1</a>
-                            <a href="#" class="navbar-item navbar-link">Item 3-2</a>
-                        </div>
-                    </div>
-                     
-                    <!--Single Menu items-->
-                    <a class="navbar-item navbar-link is-arrowless" tabindex="0" href="#">
-                        News
-                        <!--  <span class="icon"><ExternalLink /></span>-->
-                        <i class="icon external-link" v-if="isExternal" />
-                    </a>
-
-                     <!--Single Menu items-->
-                    <a class="navbar-item navbar-link is-arrowless" tabindex="0" href="#">
-                        Support us
-                        <!--  <span class="icon"><ExternalLink /></span>-->
-                        <i class="icon external-link" v-if="isExternal" />
-                    </a>
                 </div>
             </div>
         </nav>
@@ -97,8 +48,14 @@
 </template>
 <script>
     import { defineComponent } from 'vue';
+    const axios = require('axios');
     export default defineComponent({
         name: 'cc-golbal-header',
+        beforeCreate() {
+             axios.get("http://localhost:8000/wp-json/ccnavigation-header/menu")
+                .then(response => this.menus = response.data)
+                .then(menus => console.log(menus));
+        },
         props: {
             ariaPrimaryLabel: {
                 type: String,
@@ -109,7 +66,12 @@
                 default: "Menu",
             },
         },
-        data: () => ({ isBurgerMenuActive: false }),
+        data() { 
+            return {
+                isBurgerMenuActive: false,
+                menus: {} 
+            }
+        },
         methods: {
             toggleBurgerActive() {
                 this.isBurgerMenuActive = !this.isBurgerMenuActive;
