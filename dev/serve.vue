7a190df06d6bbd5950b09f9fe05133d14c9f1edc
Muluh MG Godson <godsonmuluh@gmail.com>
2021-12-01 10:07:25 +0100

    add sfc rollup files

---
+++ dev/serve.vue
@@ -0,0 +1,24 @@
+<script>
+import { defineComponent } from 'vue';
+import CcExplore from '../src/lib-components/cc-explore.vue';
+import CcGlobalFooter from '../src/lib-components/cc-global-footer.vue';
+import CcGlobalHeader from '../src/lib-components/cc-global-header.vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { CcGlobalsSample } from '@/entry.esm';
+
+export default defineComponent({
+  components: { CcExplore, CcGlobalHeader, CcGlobalFooter },
+  name: 'ServeDev',
+  // components: {
+  //  CcGlobalsSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <cc-explore />
+    <cc-global-header />
+    <cc-global-footer />
+  </div>
+</template>
